a048e689a2c35428d1e57ce465e45f872fd19619
Saúl <alu0101405810@ull.edu.es>
2023-11-15 21:24:42 +0000

    Data models

---
+++ front-end/common/classes/resource.ts
@@ -0,0 +1,7 @@
+export class Resource {
+  private resource_id: number;
+  private title: string;
+  private description: string;
+
+  constructor(title: string, description: string) {}
+}
--- front-end/common/classes/user.ts
+++ front-end/common/classes/user.ts
@@ -1,7 +1,7 @@
 import { check_user_data } from "../assets/functions/check_user";
 import { user_data } from "../interfaces/user_data";
 
-class User implements user_data {
+export class User {
   private user_id: string;
   private full_name: string;
   private birth_date: Date;
@@ -14,7 +14,12 @@ class User implements user_data {
     mail: string
   ) {
     if (!check_user_data(user_id, full_name, birth_date, mail)) {
-      throw new Error("User data input error");
+      throw new Error("ARGUMENT_ERROR");
     }
+
+    this.user_id = user_id;
+    this.full_name = full_name;
+    this.birth_date = birth_date;
+    this.mail = mail;
   }
 }
