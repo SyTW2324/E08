8e0eac2e1794014868a78a88410055669f40a010
Saúl <alu0101405810@ull.edu.es>
2023-11-28 23:48:39 +0000

    Responsive nav bar done

--- front-end/src/layouts/default/AppBar.vue
+++ front-end/src/layouts/default/AppBar.vue
@@ -3,21 +3,23 @@
     <v-app-bar-title class="nav-bar-title">E-Biblio</v-app-bar-title>
     <v-spacer></v-spacer>
     <v-app-bar-nav-icon
-      class="nav-bar-icon"
+      class="nav-bar-icon hidden-md-and-up"
       variant="text"
       @click.stop="drawer = !drawer"
     ></v-app-bar-nav-icon>
+    <v-btn
+      v-for="item in items"
+      :key="item.title"
+      :to="item.route"
+      class="nav-bar-btn hidden-sm-and-down"
+    >
+      {{ item.title }}
+    </v-btn>
   </v-app-bar>
-  <v-navigation-drawer v-model="drawer" location="right" temporary>
+  <v-navigation-drawer v-model="drawer" location="left" temporary>
     <v-list>
-      <v-list-item to="/">
-        <v-list-item-title>Home</v-list-item-title>
-      </v-list-item>
-      <v-list-item to="/signup">
-        <v-list-item-title>Sign Up</v-list-item-title>
-      </v-list-item>
-      <v-list-item to="/login">
-        <v-list-item-title>Log In</v-list-item-title>
+      <v-list-item v-for="item in items" :key="item.title" :to="item.route">
+        <v-list-item-title>{{ item.title }}</v-list-item-title>
       </v-list-item>
     </v-list>
   </v-navigation-drawer>
